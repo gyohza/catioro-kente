b37d296a85d457d8188d45fc20bdfa1072aea402
dan <dan.nagaoka.0@gmail.com>
2020-11-14 03:13:50 -0300

    Adjust style and behavior
    - Make sidenav close when route changes;
    - Change style of activated routerLink.

--- src/app/core/layout/nav/nav.component.ts
+++ src/app/core/layout/nav/nav.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +19,15 @@ export class NavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {}
+  
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      this.sidenav.close();
+    });
+  }
 
 }
